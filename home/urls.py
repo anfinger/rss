81c19394c05cc578c03d004d846e7e79635e7232
anfinger <finger.a@gmx.de>
2015-11-19 12:59:25 +0100

    Added views to create/edit blog post inside the site.

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.aktuelles, name='aktuelles'),
     url(r'^aktuell/(?P<pk>[0-9]+)/$', views.aktuell_detail, name='aktuell_detail'),
+    url(r'^new/$', views.aktuell_neu, name='aktuell_neu'),
+    url(r'^aktuell/(?P<pk>[0-9]+)/edit/$', views.aktuell_edit, name='aktuell_edit'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from .forms import AktuellesForm
+
 #from django.http import HttpResponse
 
 from .models import Aktuelles
@@ -11,3 +13,30 @@ def aktuelles(request):
 def aktuell_detail(request, pk):
     aktuell = get_object_or_404(Aktuelles, pk=pk)
     return render(request, 'home/aktuell_detail.html', {'aktuell': aktuell})
+
+def aktuell_neu(request):
+    if request.method == "POST":
+        form = AktuellesForm(request.POST)
+        if form.is_valid():
+            aktuell = form.save(commit=False)
+            aktuell.author = request.user
+            aktuell.published_date = timezone.now()
+            aktuell.save()
+            return redirect('home.views.aktuell_detail', pk=aktuell.pk)
+    else:
+        form = AktuellesForm()
+    return render(request, 'home/aktuell_edit.html', {'form': form})
+
+def aktuell_edit(request, pk):
+    aktuell = get_object_or_404(Aktuelles, pk=pk)
+    if request.method == "POST":
+        form = AktuellesForm(request.POST, instance=aktuell)
+        if form.is_valid():
+            aktuell = form.save(commit=False)
+            aktuell.author = request.user
+            aktuell.published_date = timezone.now()
+            aktuell.save()
+            return redirect('home.views.aktuell_detail', pk=aktuell.pk)
+    else:
+        form = AktuellesForm(instance=aktuell)
+    return render(request, 'home/aktuell_edit.html', {'form': form})
