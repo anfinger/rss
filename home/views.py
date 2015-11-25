dc95d18676059ef34940e129616532e253a2c2a0
anfinger <finger.a@gmx.de>
2015-11-25 22:36:07 +0100

    login und so

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 from .forms import AktuellesForm
 
 #from django.http import HttpResponse
@@ -14,19 +15,22 @@ def aktuell_detail(request, pk):
     aktuell = get_object_or_404(Aktuelles, pk=pk)
     return render(request, 'home/aktuell_detail.html', {'aktuell': aktuell})
 
+@login_required
 def aktuell_neu(request):
     if request.method == "POST":
         form = AktuellesForm(request.POST)
         if form.is_valid():
             aktuell = form.save(commit=False)
             aktuell.author = request.user
-            aktuell.published_date = timezone.now()
+            # wenn kein draft erzeugt werden soll, sondern gleich publiziert werden soll dann published_date setzen
+            # aktuell.published_date = timezone.now()
             aktuell.save()
             return redirect('home.views.aktuell_detail', pk=aktuell.pk)
     else:
         form = AktuellesForm()
     return render(request, 'home/aktuell_edit.html', {'form': form})
 
+@login_required
 def aktuell_edit(request, pk):
     aktuell = get_object_or_404(Aktuelles, pk=pk)
     if request.method == "POST":
@@ -40,3 +44,20 @@ def aktuell_edit(request, pk):
     else:
         form = AktuellesForm(instance=aktuell)
     return render(request, 'home/aktuell_edit.html', {'form': form})
+
+@login_required
+def aktuell_draft_list(request):
+    aktuelles = Aktuelles.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'home/aktuell_draft_list.html', {'aktuelles': aktuelles})
+
+@login_required
+def aktuell_publish(request, pk):
+    aktuell = get_object_or_404(Aktuelles, pk=pk)
+    aktuell.publish()
+    return redirect('home.views.aktuell_detail', pk=pk)
+
+@login_required
+def aktuell_remove(request, pk):
+    aktuell = get_object_or_404(Aktuelles, pk=pk)
+    aktuell.delete()
+    return redirect('home.views.aktuelles')
